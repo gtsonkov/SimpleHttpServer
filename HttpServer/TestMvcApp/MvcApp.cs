0de77b205fa7df7db6a470ab83f3ed5798ef42df
Georgi Tsonkov <gtsonkov@abv.bg>
2020-10-07 22:14:39 +0200

    -respons for favico

--- HttpServer/TestMvcApp/MvcApp.cs
+++ HttpServer/TestMvcApp/MvcApp.cs
@@ -1,6 +1,7 @@
 ﻿using HttpServer.Http;
 using HttpServer.Http.Constants;
 using HttpServer.Http.Contracts;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace TestMvcApp
             IHttpServer server = new Server();
             server.AddRoute("/", HomePage);
             server.AddRoute("/about", About);
+            server.AddRoute("/favicon.ico", Favicon);
             await server.StartAsync(80);
         }
 
@@ -40,6 +42,13 @@ namespace TestMvcApp
             var response = new HttpResponse(MessagesResponse.ContentHTML, responseBodyBytes);
             response.Headers.Add(new Header(ConstantData.ServerNameHeader, MessagesResponse.FullServerInfo));
 
+            return response;
+        }
+
+        static HttpResponse Favicon(HttpRequest request)
+        {
+            var fileBytes = File.ReadAllBytes("wwwroot/favicon.ico");
+            var response = new HttpResponse("image/vnd.microsoft.icon", fileBytes);
             return response;
         }
 
