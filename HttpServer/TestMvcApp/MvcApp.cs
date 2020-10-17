b3090ade77db9f481afd3e1d0a7aba23f8cb4936
Georgi Tsonkov <gtsonkov@abv.bg>
2020-10-17 17:19:54 +0200

    change route paths

--- HttpServer/TestMvcApp/MvcApp.cs
+++ HttpServer/TestMvcApp/MvcApp.cs
@@ -11,10 +11,10 @@ namespace TestMvcApp
         {
             IHttpServer server = new Server();
             server.AddRoute("/",new HomeController().Index);
-            server.AddRoute("/about",new HomeController().About);
+            server.AddRoute("/home/about",new HomeController().About);
             server.AddRoute("/favicon.ico",new StaticFilesController().Favicon);
-            server.AddRoute("/login", new UsersController().Login);
-            server.AddRoute("/register", new UsersController().Register);
+            server.AddRoute("/users/login", new UsersController().Login);
+            server.AddRoute("/users/register", new UsersController().Register);
 
             await server.StartAsync(80);
         }
