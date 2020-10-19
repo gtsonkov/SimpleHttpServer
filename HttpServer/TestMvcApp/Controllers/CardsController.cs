20a726964d9baafe1cfaff3fa1170e7f5a888bdb
Georgi Tsonkov <gtsonkov@abv.bg>
2020-10-19 20:07:11 +0200

    -CardsController -add Actions

--- HttpServer/TestMvcApp/Controllers/CardsController.cs
+++ HttpServer/TestMvcApp/Controllers/CardsController.cs
@@ -6,7 +6,19 @@ namespace TestMvcApp.Controllers
 {
     public class CardsController : Controller
     {
+        public HttpResponse All(HttpRequest request)
+        {
+            string path = Generator.GenerateInternPath(request.Path);
+            return this.Viev(path);
+        }
+
         public HttpResponse Add(HttpRequest request)
+        {
+            string path = Generator.GenerateInternPath(request.Path);
+            return this.Viev(path);
+        }
+
+        public HttpResponse Collection(HttpRequest request)
         {
             string path = Generator.GenerateInternPath(request.Path);
             return this.Viev(path);
--- HttpServer/TestMvcApp/MvcApp.cs
+++ HttpServer/TestMvcApp/MvcApp.cs
@@ -10,11 +10,15 @@ namespace TestMvcApp
         static async Task Main(string[] args)
         {
             IHttpServer server = new Server();
+
             server.AddRoute("/",new HomeController().Index);
             server.AddRoute("/Home/About",new HomeController().About);
             server.AddRoute("/favicon.ico",new StaticFilesController().Favicon);
             server.AddRoute("/Users/Login", new UsersController().Login);
             server.AddRoute("/Users/Register", new UsersController().Register);
+            server.AddRoute("/Cards/All", new CardsController().All);
+            server.AddRoute("/Cards/Add", new CardsController().Add);
+            server.AddRoute("/Cards/Collection", new CardsController().Collection);
 
             await server.StartAsync(80);
         }
