4f4160c7c3c9dd54f96d152f13961bf4705514fb
Georgi Tsonkov <gtsonkov@abv.bg>
2020-10-31 20:05:36 +0100

    -implement GetMethodBody method

--- HttpServer/HttpServer.MvcFramework/ViewEngine/SusViewEngine.cs
+++ HttpServer/HttpServer.MvcFramework/ViewEngine/SusViewEngine.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace HttpServer.MvcFramework.ViewEngine
 {
@@ -51,12 +52,12 @@ namespace HttpServer.MvcFramework.ViewEngine
         {
             string usings = GetUsings(typeof(CsharpLibs));
 
-            string csharpCode = usings 
+            string csharpCode = usings
                 + Environment.NewLine
                 + File.ReadAllText(
-                  CodeTemplatePart1Path 
+                  CodeTemplatePart1Path
                 + Environment.NewLine
-                + GetMethodBody(templateCode) 
+                + GetMethodBody(templateCode)
                 + Environment.NewLine
                 + CodeTemplatePart2Path);
 
@@ -69,7 +70,7 @@ namespace HttpServer.MvcFramework.ViewEngine
                 .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)) //What kind of projekt will be created, in this case will be created DLL
                 .AddReferences(MetadataReference.CreateFromFile(typeof(object).Assembly.Location)) //.Net Base lilbrary will be refered to this project
                 .AddReferences(MetadataReference.CreateFromFile(typeof(IView).Assembly.Location)); // The HttpServer.MvcFramework Lib will be refered
-            
+
             if (viewModel != null)
             {
                 compileResult = compileResult.AddReferences(MetadataReference.CreateFromFile(viewModel.GetType().Assembly.Location));
@@ -93,7 +94,7 @@ namespace HttpServer.MvcFramework.ViewEngine
 
             using (MemoryStream memStream = new MemoryStream())
             {
-               var compilationResult = compileResult.Emit(memStream);
+                var compilationResult = compileResult.Emit(memStream);
 
                 if (!compilationResult.Success)
                 {
@@ -119,6 +120,53 @@ namespace HttpServer.MvcFramework.ViewEngine
             }
         }
 
-        private string GetMethodBody(string templateCode) => throw new NotImplementedException();
+        private string GetMethodBody(string templateCode)
+        {
+            Regex csharpCodeRegex = new Regex(@"[^\""\s&\'\<]+");
+            var supportedOperators = new List<string> { "for", "while", "if", "else", "foreach" };
+
+            StringBuilder csharpCode = new StringBuilder();
+            StringReader sr = new StringReader(templateCode);
+
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (supportedOperators.Any(x => line.TrimStart().StartsWith("@" + x)))
+                {
+                    var atSignLocation = line.IndexOf("@");
+                    line = line.Remove(atSignLocation, 1);
+                    csharpCode.AppendLine(line);
+                }
+
+                else if (line.TrimStart().StartsWith("{") || line.TrimStart().StartsWith("}"))
+                {
+                    csharpCode.AppendLine(line);
+                }
+
+                else
+                {
+                    csharpCode.Append($"html.AppendLine(@\"");
+
+                    while (line.Contains("@"))
+                    {
+                        var atSignLocation = line.IndexOf("@");
+                        var htmlBeforeAtSign = line.Substring(0, atSignLocation);
+
+                        csharpCode.Append(htmlBeforeAtSign.Replace("\"", "\"\"") + "\" + ");
+
+                        var lineAfterAtSign = line.Substring(atSignLocation + 1);
+                        var code = csharpCodeRegex.Match(lineAfterAtSign).Value;
+
+                        csharpCode.Append(code + " + @\"");
+                        line = lineAfterAtSign.Substring(code.Length);
+                    }
+
+                    csharpCode.AppendLine(line.Replace("\"", "\"\"") + "\");");
+                }
+            }
+
+            return csharpCode.ToString();
+        }
     }
 }
