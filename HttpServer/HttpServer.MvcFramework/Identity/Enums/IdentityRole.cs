7c43f9f2fb28ed88d6fac126d20ad225e2d0084d
Georgi Tsonkov <gtsonkov@abv.bg>
2020-11-01 22:11:27 +0100

    -implement Identity

---
+++ HttpServer/HttpServer.MvcFramework/Identity/Enums/IdentityRole.cs
@@ -0,0 +1,8 @@
+﻿namespace HttpServer.MvcFramework.Identity.Enums
+{
+    public enum IdentityRole
+    {
+        User = 1,
+        Admin = 2,
+    }
+}
---
+++ HttpServer/HttpServer.MvcFramework/Identity/IdentityUser.cs
@@ -0,0 +1,17 @@
+﻿using HttpServer.MvcFramework.Identity.Enums;
+
+namespace SUS.MvcFramework
+{
+    public class IdentityUser<T>
+    {
+        public T Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public IdentityRole Role { get; set; }
+    }
+}
